6366867beeb3d180bd2fa79f379bbbb10f77f3e4
cepeasbhy <asbhycepe01@gmail.com>
2020-12-06 19:00:16 +0800

    Added loop to process multiple sets of inputs

--- prob2.java
+++ prob2.java
@@ -7,35 +7,44 @@ public class prob2 {
 		Scanner input = new Scanner(System.in);
 		int[] num = new int[3];
 		
-		System.out.print("Enter value for A, B, C[Separated by space]: ");
+		System.out.println("Enter number of inputs to be processed: ");
+		int run  = input.nextInt();
+		input.nextLine();
 		
-		String in  = input.nextLine();
-		
-		String[] get_val = in.split(" ");
-		
-		int a = Integer.parseInt(get_val[0]);
-		int b = Integer.parseInt(get_val[1]);
-		int c = Integer.parseInt(get_val[2]);
-		
-		System.out.println("QE: "+a+"x^2 + "+b+ "x +"+ c);
-		
-		int discriminat = ((b*b)-(4*a*c));
-		double root = Math.sqrt(discriminat);
-		
-		if(a == 0) {
-			System.out.println("Not a valid quadratic equation");
+		for(int i = 1; i<=run; i++) {
 			
-		}else if(discriminat > 0) {
-			double x1 = (-b+root)/(2*a);
-			double x2 = (-b-root)/(2*a);
-			System.out.print("x1 = " + x1 + " x2 = "+ x2);
+			System.out.println("Input"+i);
+			System.out.print("Enter value for A, B, C[Separated by space]: ");
 			
-		}else if(discriminat == 0) {
-			double result = -b/(2*a);
-			System.out.print("x = "+result);
+			String in  = input.nextLine();
 			
-		}else{
-			System.out.print("x = IMAGINARY");
+			String[] get_val = in.split(" ");
+			
+			int a = Integer.parseInt(get_val[0]);
+			int b = Integer.parseInt(get_val[1]);
+			int c = Integer.parseInt(get_val[2]);
+			
+			System.out.println("QE: "+a+"x^2 + "+b+ "x +"+ c);
+			
+			int discriminat = ((b*b)-(4*a*c));
+			double root = Math.sqrt(discriminat);
+			
+			if(a == 0) {
+				System.out.println("Not a valid quadratic equation");
+				
+			}else if(discriminat > 0) {
+				double x1 = (-b+root)/(2*a);
+				double x2 = (-b-root)/(2*a);
+				System.out.printf("x1 = " + x1 + " x2 = "+ x2);
+				
+			}else if(discriminat == 0) {
+				double result = -b/(2*a);
+				System.out.print("x = "+result);
+				
+			}else{
+				System.out.print("x = IMAGINARY");
+				
+			}
 			
 		}
 		
