8902abf79b4f3f5442eeb8fb3f5f8b93f921693c
cepeasbhy <asbhycepe01@gmail.com>
2020-12-06 17:52:52 +0800

    FIRST COMMIT

---
+++ prob2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class prob2 {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int[] num = new int[3];
+		
+		System.out.print("Enter value for A, B, C[Separated by space]: ");
+		
+		String in  = input.nextLine();
+		
+		String[] get_val = in.split(" ");
+		
+		for(int i = 0; i<3; i++) {
+			
+		}
+	}
+		
+}
