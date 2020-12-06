72a3d0eaa5b5d3b9d67aa0aa8a587ed817de12d1
cepeasbhy <asbhycepe01@gmail.com>
2020-12-06 18:35:52 +0800

    Added display for QE and condition for when a is 0

--- prob2.java
+++ prob2.java
@@ -19,17 +19,19 @@ public class prob2 {
 		int b = Integer.parseInt(get_val[1]);
 		int c = Integer.parseInt(get_val[2]);
 		
+		System.out.println("QE: "+a+"x^2 + "+b+ "x +"+ c);
+		
 		int discriminat = ((b*b)-(4*a*c));
 		double root = Math.sqrt(discriminat);
 		
-		if(discriminat > 0) {
+		if(a == 0) {
+			System.out.println("Not a valid quadratic equation");
+			
+		}else if(discriminat > 0) {
 			double x1 = (-b+root)/(2*a);
 			double x2 = (-b-root)/(2*a);
 			System.out.print("x1 = " + x1 + " x2 = "+ x2);
 		}
-		
-		
-		
 		
 	}
 		
